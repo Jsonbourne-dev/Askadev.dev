faf144fe74da69c8ffea3da6264de9f5be6ef82e
duncannevin <duncan.nevin@gmail.com>
2024-08-20 18:07:52 -0700

    Add redux devtools config

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import questionsReducer from './reducers/questionsReducer';
 
-const rootReducer = combineReducers({
+const reducers = combineReducers({
   questions: questionsReducer,
 });
 
-const store = createStore(rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware()))
 
 export default store;
