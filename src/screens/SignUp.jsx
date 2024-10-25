4502c2f4e535abb0377edd7c73ea14c344a8d9ac
Jsonbourne-dev <eli@orange.me>
2024-10-25 10:41:40 -0600

    Added FireBase auth for google and password/email

--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux"; 
+import { useDispatch } from "react-redux"; 
 import styled from "styled-components";
-import { v4 as uuidv4 } from 'uuid'; 
 import { Link } from "react-router-dom"; 
-import { setUser, signInWithToken } from '../redux/actions/userActions'; 
+import { setUser } from '../redux/actions/userActions'; 
 import { InputField } from "../styled-components"; 
 import StyledButton from '../styled-components/button'; 
-import { FiCopy } from 'react-icons/fi'; 
 import SignInLeftSection from '../components/SignInLeftSection';
 import Logo from '../assets/logo-green.svg'; 
+import { auth, provider } from '../firebase/firebase'; 
+import { signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"; 
+import GoogleLogo from '../assets/googlelogo.png'; 
 
 const Body = styled.div`
   display: flex;
@@ -18,10 +19,6 @@ const Body = styled.div`
   position: relative;
   width: 100vw;
   font-family: 'Space Grotesk', sans-serif; 
-
-  @media (max-width: 1400px) {
-    justify-content: center;
-  }
 `;
 
 const LeftHalf = styled.div`
@@ -81,14 +78,6 @@ const FormTitle = styled.h3`
   font-size: 32px;
 `;
 
-const TokenTitle = styled.h3`
-  color: #fff;
-  margin-bottom: 40px;
-  font-size: 40px;
-  text-align: center;  
-  width: 900px;
-`;
-
 const Spacer = styled.div`
   height: 20px;
 `;
@@ -99,42 +88,6 @@ const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
   padding: 0 10px; 
-`;
-
-const TokenContainer = styled.div`
-  width: 100%;
-  max-width: 400px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  background-color: #fff;
-  border-radius: 4px;
-  padding: 10px;
-  font-size: 16px;
-  margin-bottom: 10px;
-`;
-
-const TokenInput = styled.input`
-  border: none;
-  background: transparent;
-  color: #000;
-  font-size: 16px;
-  width: 100%;
-  margin-right: 10px;
-  &:focus {
-    outline: none;
-  }
-`;
-
-const CopyIcon = styled(FiCopy)`
-  cursor: pointer;
-  color: #000;
-`;
-
-const CopiedMessage = styled.span`
-  color: green;
-  margin-left: 10px;
-  font-size: 14px;
 `;
 
 const ErrorText = styled.p`
@@ -159,20 +112,44 @@ const SwitchLink = styled.p`
   text-align: center; 
 `;
 
+const UserAuthSection = styled.div`
+  width: 100%;
+  margin-bottom: 30px;
+`;
+
+const GoogleSignInButton = styled.button`
+  background-color: #f4f4f4; /* Whitish-grey background */
+  color: black; /* Black text */
+  border: none; /* No border */
+  border-radius: 5px; /* Rounded corners */
+  padding: 10px 15px; /* Padding for button */
+  display: flex; /* Flex display to align items */
+  align-items: center; /* Center align items vertically */
+  cursor: pointer; /* Pointer cursor on hover */
+  width: 100%; /* Full width of the container */
+  margin-top: 10px; /* Spacing above the button */
+  font-size: 16px; /* Adjust font size as needed */
+
+  &:hover {
+    background-color: #e0e0e0; /* Slightly darker on hover */
+  }
+`;
+
+const GoogleLogoImage = styled.img`
+  width: 20px; /* Logo width */
+  margin-right: 10px; /* Space between logo and text */
+`;
+
 const SignUp = () => {
   const dispatch = useDispatch(); 
-  const storedUser = useSelector(state => state.user); 
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [isAccountCreated, setIsAccountCreated] = useState(false);
-  const [token, setToken] = useState("");
-  const [isCopied, setIsCopied] = useState(false); 
   const [isSignIn, setIsSignIn] = useState(false); 
 
-  const handleCreateAccount = () => {
+  const handleCreateAccount = async () => {
     if (username.length > 10) {
       setErrorMessage("Username must not exceed 10 characters.");
       return;
@@ -182,44 +159,60 @@ const SignUp = () => {
       return;
     }
 
-    if (!username || !email || !password || !confirmPassword) {
+    if (!username || !email || !password) {
       setErrorMessage("Please fill in all fields");
       return;
     }
-    if (password !== confirmPassword) {
-      setErrorMessage("Passwords do not match");
+
+    // Check for password length
+    if (password.length < 10) {
+      setErrorMessage("Password must be at least 10 characters long.");
       return;
     }
 
-    const newToken = uuidv4();
-    setToken(newToken);
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
 
-    dispatch(setUser(username, email, password, newToken)); 
-
-    console.log("Creating account with:", username, email, password);
-    setIsAccountCreated(true);
-  };
-
-  const handleCopyToken = () => {
-    navigator.clipboard.writeText(token);
-    setIsCopied(true); 
-
-    setTimeout(() => {
-      setIsCopied(false);
-    }, 2000);
-  };
-
-  const handleSignIn = () => {
-    if (!storedUser || storedUser.username !== username || storedUser.token !== token) {
-      setErrorMessage("Account not found. Please check your username and token.");
-      return;
+      // Dispatch user creation action
+      dispatch(setUser(username, user.email, password, user.uid)); 
+      setIsAccountCreated(true);
+      console.log("Account created:", username, user.email);
+    } catch (error) {
+      setErrorMessage(error.message);
     }
+  };
 
-    dispatch(signInWithToken(username, token));
+  const handleGoogleSignIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
 
-    console.log("Signing in with:", username, token);
-    
-    window.location.href = '/#/community'; 
+      const username = user.displayName.split(' ')[0]; // Get first name as username
+
+      // Dispatch user creation action
+      dispatch(setUser(username, user.email, "", user.uid)); 
+      console.log("Signed in with Google:", username, user.email);
+
+      window.location.href = '/#/community'; 
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  const handleEmailSignIn = async () => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      // Dispatch user login action
+      dispatch(setUser(user.displayName, user.email, "", user.uid)); 
+      console.log("Signed in with email:", user.email);
+
+      window.location.href = '/#/community'; 
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
   };
 
   return (
@@ -239,23 +232,29 @@ const SignUp = () => {
               <FormTitle>Sign In</FormTitle>
               <InputContainer>
                 <InputField
-                  type="text"
-                  placeholder="Username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
+                  type="email"
+                  placeholder="Email Address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
                 <Spacer />
                 <InputField
-                  type="text"
-                  placeholder="Access Token"
-                  value={token}
-                  onChange={(e) => setToken(e.target.value)}
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
                 <Spacer />
                 {errorMessage && <ErrorText>{errorMessage}</ErrorText>}
-                <StyledButton variant="desktop-filled" onClick={handleSignIn}>
+                <StyledButton variant="desktop-filled" onClick={handleEmailSignIn}>
                   Sign In
                 </StyledButton>
+                <Spacer />
+                <GoogleSignInButton onClick={handleGoogleSignIn}>
+                  <GoogleLogoImage src={GoogleLogo} alt="Google logo" />
+                  Sign In with Google
+                </GoogleSignInButton>
+                <Spacer />
                 <SwitchLink onClick={() => setIsSignIn(false)}>Create an account</SwitchLink>
               </InputContainer>
             </>
@@ -263,14 +262,8 @@ const SignUp = () => {
             <>
               {isAccountCreated ? (
                 <>
-                  <TokenTitle>Your Token of Authentication.</TokenTitle>
-                  <TokenContainer>
-                    <TokenInput value={token} readOnly />
-                    <CopyIcon onClick={handleCopyToken} />
-                    {isCopied && <CopiedMessage>Copied!</CopiedMessage>}
-                  </TokenContainer>
                   <p style={{ color: "#ff0000", textAlign: "center" }}>
-                    This is your token. Don't lose it, or you'll lose access to your data.
+                    Account created successfully! You can now log in.
                   </p>
                   <ContinueButtonContainer>
                     <Link to="/#/community" style={{ textDecoration: 'none' }}>
@@ -283,53 +276,42 @@ const SignUp = () => {
               ) : (
                 <>
                   <FormTitle>Sign Up</FormTitle>
-                  <InputContainer>
-                    <InputField
-                      type="text"
-                      placeholder="Username"
-                      value={username}
-                      onChange={(e) => {
-                        const newUsername = e.target.value;
-                        setUsername(newUsername);
-
-                        
-                        if (newUsername.length > 10) {
-                          setErrorMessage("Username must not exceed 10 characters.");
-                        } else if (/\s/.test(newUsername)) {
-                          setErrorMessage("Username must not contain spaces.");
-                        } else {
-                          setErrorMessage(""); 
-                        }
-                      }}
-                    />
-                    <Spacer />
-                    <InputField
-                      type="email"
-                      placeholder="Email Address"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <Spacer />
-                    <InputField
-                      type="password"
-                      placeholder="Password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <Spacer />
-                    <InputField
-                      type="password"
-                      placeholder="Confirm Password"
-                      value={confirmPassword}
-                      onChange={(e) => setConfirmPassword(e.target.value)}
-                    />
-                    <Spacer />
-                    {errorMessage && <ErrorText>{errorMessage}</ErrorText>}
-                    <StyledButton variant="desktop-filled" onClick={handleCreateAccount}>
-                      Create Account
-                    </StyledButton>
-                    <SwitchLink onClick={() => setIsSignIn(true)}>Log in</SwitchLink>
-                  </InputContainer>
+                  <UserAuthSection>
+                    <InputContainer>
+                      <InputField
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                      />
+                      <Spacer />
+                      <InputField
+                        type="email"
+                        placeholder="Email Address"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <Spacer />
+                      <InputField
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      <Spacer />
+                      {errorMessage && <ErrorText>{errorMessage}</ErrorText>}
+                      <StyledButton variant="desktop-filled" onClick={handleCreateAccount}>
+                        Create Account
+                      </StyledButton>
+                      <Spacer />
+                      <GoogleSignInButton onClick={handleGoogleSignIn}>
+                        <GoogleLogoImage src={GoogleLogo} alt="Google logo" />
+                        Sign In with Google
+                      </GoogleSignInButton>
+                      <Spacer />
+                      <SwitchLink onClick={() => setIsSignIn(true)}>Log in</SwitchLink>
+                    </InputContainer>
+                  </UserAuthSection>
                 </>
               )}
             </>
